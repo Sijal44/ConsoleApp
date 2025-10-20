4ea5ec09a70eaab461a02577e5102708e54f6a03
Sijal44 <sajad.azizi92@yahoo.com>
2025-10-20 20:50:52 +0200

    Added JobApplication class and AddJob method

--- JobManager.cs
+++ JobManager.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
-    internal class JobManager
+    public class JobManager
     {
+        //Attributer :
+        //Applications Samling av alla ansökningar
+        List<JobApplication> Applications = new List<JobApplication>();
+
+        //======================metoder========================
+
+       //AddJob() – lägger till en ny ansökan
+        public void AddJob()
+        {
+            JobApplication newJob = new JobApplication();
+            Console.WriteLine("Ange företagsnamn:");
+            newJob.CompanyName = Console.ReadLine();
+
+            Console.WriteLine("Ange titel:");
+            newJob.PositionTitle = Console.ReadLine();
+
+            Console.Write("Status (Applied, Interview, Offer, Rejected): ");
+            newJob.Status = Console.ReadLine();
+
+            Console.WriteLine("Ange ansökningsdatum (YYYY-MM-DD):");
+            newJob.ApplicationDate = DateTime.Now; //aktuellt datum och tid just nu
+
+            Console.WriteLine("Ange löneförväntning: (i kronor)");
+            newJob.SalaryExpectation=Convert.ToInt32(Console.ReadLine());
+
+            Applications.Add(newJob); //sparar hela objektet med all info i listan
+            Console.WriteLine("Ansökan tillagd!");
+        }
+
+        //UpdateStatus() – ändrar status på en befintlig ansökan
+        //ShowAll() – visar alla ansökningar
     }
 }
