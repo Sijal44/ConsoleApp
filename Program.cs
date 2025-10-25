2ede52b01f4f6311aadf216f7fb881c87a213819
Sijal44 <sajad.azizi92@yahoo.com>
2025-10-25 18:32:58 +0200

    just added Try

--- Program.cs
+++ Program.cs
@@ -1,50 +1,58 @@
 ﻿namespace ConsoleApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            JobManager manager=new JobManager();
+            // skapade instance of JobManager
+            JobManager manager = new JobManager();
+
             bool run = true;
 
             while (run)
             {
-                Console.WriteLine("===============================");
-                Console.WriteLine("    Job Application Tracker    ");
-                Console.WriteLine("===============================");
-                Console.WriteLine("1. Lägg till ny ansökan");
-                Console.WriteLine("2. Ta bort ansökan");
-                Console.WriteLine("3. Visa alla ansökningar");
-                Console.WriteLine("4. Uppdatera status");
-                Console.WriteLine("5. Avsluta programmet");
-                Console.WriteLine();
-                Console.Write("Välj ett alternativ: ");
-
-                string choice = Console.ReadLine();
-                Console.Clear();
-
-
-                switch (choice)
+                try
                 {
-                    case "1":
-                        manager.AddJob();
-                        break;
-                    case "2":
-                        manager.DeleteJob();
-                        break;
-                    case "3":
-                        manager.ShowAll();
-                        break;
-                    case "4":
-                        manager.UpdateStatus();
-                        break;
-                    case "5":
-                        run = false;
-                        Console.WriteLine("Programmet avslutas...");
-                        break;
-                    default:
-                        Console.WriteLine("Ogiltigt val, försök igen.");
-                        break;
+                    Console.WriteLine("===========================");
+                    Console.WriteLine("  Job Application Tracker  ");
+                    Console.WriteLine("===========================");
+                    Console.WriteLine(" 1. Lägg till ny ansökan.");
+                    Console.WriteLine(" 2. Ta bort ansökan.");
+                    Console.WriteLine(" 3. Visa alla ansökningar.");
+                    Console.WriteLine(" 4. Uppdatera status.");
+                    Console.WriteLine(" 5. Avsluta programmet.");
+                    Console.WriteLine();
+                    Console.Write("-Välj ett alternativ: ");
+
+                    string choice = Console.ReadLine();
+                    Console.Clear();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            manager.AddJob();
+                            break;
+                        case "2":
+                            manager.DeleteJob();
+                            break;
+                        case "3":
+                            manager.ShowAll();
+                            break;
+                        case "4":
+                            manager.UpdateStatus();
+                            break;
+                        case "5":
+                            run = false;
+                            Console.WriteLine("Programmet avslutas...");
+                            break;
+                        default:
+                            Console.WriteLine("Ogiltigt val, försök igen.");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Fel format! Skriv datum så här: YYYY-MM-DD,");
                 }
             }
         }
